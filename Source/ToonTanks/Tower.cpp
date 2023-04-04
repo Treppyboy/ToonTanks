8c4dcd9a765131391b69d5e75206b34737a460e1
Treppyboy <50812505+Treppyboy@users.noreply.github.com>
2023-04-04 00:53:45 -0400

    Created the Tower cpp class
    end of lecture 131. the tower cpp class is created, and rotates towards player if in FireRange. Added Debug spere to tweak FireRange

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -2,4 +2,31 @@
 
 
 #include "Tower.h"
+#include "Tank.h"
+#include "kismet/GameplayStatics.h"
 
+void ATower::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    DrawDebugSphere(GetWorld(), GetActorLocation(), FireRange, 24, FColor::Red, false, -1.0f);
+
+    if (Tank)
+    {
+        // find distance to the tank
+        float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+        // check to see if tank is in range
+        if (Distance <= FireRange)
+        {
+            RotateTurret(Tank-> GetActorLocation());
+        }
+        
+    }
+}
+
+void ATower::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this,0));
+}
--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -13,5 +13,18 @@ UCLASS()
 class TOONTANKS_API ATower : public ABasePawn
 {
 	GENERATED_BODY()
-	
+
+
+public: 
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	class ATank* Tank;
+
+	UPROPERTY(EditDefaultsOnly,Category = "Combat")
+	float FireRange = 300.f;
 };

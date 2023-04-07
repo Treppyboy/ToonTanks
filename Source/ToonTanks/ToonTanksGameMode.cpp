9b384a4f4a465dd1acc9a67c5a488eaba6696d25
Treppyboy <50812505+Treppyboy@users.noreply.github.com>
2023-04-07 18:35:47 -0400

    end of lecture 143
    Ftimer set so that the tank cannot move for 3 seconds.

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -7,6 +7,7 @@
 #include "Tower.h"
 #include "ToonTanksPlayerController.h"
 
+
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 {
 
@@ -29,6 +30,26 @@ void AToonTanksGameMode::BeginPlay()
 {
     Super::BeginPlay();
 
+    HandleGameStart();
+}
+
+void AToonTanksGameMode::HandleGameStart()
+{
+    UE_LOG(LogTemp, Warning, TEXT("HandleGameStart() was called,"));
+
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
-    ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this,0));
+    ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+
+    if (ToonTanksPlayerController)
+    {
+        ToonTanksPlayerController->SetPlayerEnabledState(false);
+        
+
+        FTimerHandle PlayerEnableTimerHandle;
+        FTimerDelegate InputDelegate = FTimerDelegate::CreateUObject(ToonTanksPlayerController, &AToonTanksPlayerController::SetPlayerEnabledState,true);
+        GetWorldTimerManager().SetTimer(PlayerEnableTimerHandle, InputDelegate, StartDelay, false);
+        UE_LOG(LogTemp, Warning, TEXT("TimerStarted"));
+
+    }
+    
 }
--- Source/ToonTanks/ToonTanksPlayerController.cpp
+++ Source/ToonTanks/ToonTanksPlayerController.cpp
@@ -4,15 +4,26 @@
 #include "ToonTanksPlayerController.h"
 #include "GameFramework/Pawn.h"
 
+void AToonTanksPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    UE_LOG(LogTemp, Warning, TEXT("ToonTanksPlayerController initialized."));
+
+}
+
 void AToonTanksPlayerController::SetPlayerEnabledState(bool bPlayerEnabled)
 {
+    UE_LOG(LogTemp, Warning, TEXT("The bPlayerEnabled value is %s"), (bPlayerEnabled ? TEXT("true") : TEXT("false")));
+
     if (bPlayerEnabled)
     {
         GetPawn()->EnableInput(this);
+        UE_LOG(LogTemp, Warning, TEXT("Player EnableInput() was called"));
     }
     else
     {
         GetPawn()->DisableInput(this);
+        UE_LOG(LogTemp, Warning, TEXT("Player DisableInput() was called"));
     }
     
     bShowMouseCursor = bPlayerEnabled;
